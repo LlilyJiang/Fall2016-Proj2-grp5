f35bc836363ddba9d993479b397f342e6a8bdccd
HolyMonarch <wb2304@columbia.edu>
2016-10-08 18:21:55 -0400

    Update ui.R

--- app/ui.R
+++ app/ui.R
@@ -1,12 +1,6 @@
 library(shiny)
 library(shinydashboard)
 
-# Choices for drop-downs
-#TreeType <- c(
-#  "All"=""
-#  "Maple"="maple"
-#)
-
 
 shinyUI(dashboardPage(
   dashboardHeader(title = "Trees in New York"
@@ -15,17 +9,33 @@ shinyUI(dashboardPage(
     sidebarMenu(
       menuItem("Data Visualization",tabName="dashboard",icon= icon("dashboard")),
       menuItem("Clustering Analysis",tabName="Clustering",icon=icon("th"))
-      )
+      ),
+    
+    textInput("location","One location:",'Columbia University NY, NYC'),
+#    selectInput("Tree_types","Choose One Category",Types),
+    checkboxGroupInput("Category","Your Favorite Tree:",Types,inline=T),
+    submitButton("Apply Changes")
     ),
   dashboardBody(
     tabItems(
       tabItem(tabName="dashboard",
         fluidRow(
-          box(
-            plotOutput("maps")),
-          box(
-            plotOutput("treeIcon"))
-          )),
+          column(width=9,
+                 box(width=NULL,solidHeader = TRUE,
+              title="New York City",
+              collapsible=T,
+            leafletOutput("map")
+            )),
+          column(width=3,
+                 box(
+                   plotOutput("Tree_Icon")
+                 ),
+                 box(
+                   textOutput("Information")
+                 ),offset=0
+                 )
+          )
+            ),
       tabItem(tabName="Clustering",
         fluidRow(
           box(
