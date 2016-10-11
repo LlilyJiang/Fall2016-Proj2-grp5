1ab02da0a11b3f8008c3277824bd9f53e9ce87d3
Lily Jiang <rongyue717@gmail.com>
2016-10-10 22:54:28 -0400

    add factorize

--- output/EDA plots.R
+++ output/EDA plots.R
@@ -2,6 +2,20 @@
 #### Processing
 treedata <- treedata[!(treedata$health == ""), ]
 
+### factor into integer
+treedata$root_stone<-as.integer(treedata$root_stone=='Yes')
+treedata$root_grate<-as.integer(treedata$root_grate=='Yes')
+treedata$trunk_wire<-as.integer(treedata$trunk_wire=='Yes')
+treedata$trnk_light<-as.integer(treedata$trnk_light=='Yes')
+treedata$brch_light<-as.integer(treedata$brch_light=='Yes')
+treedata$brch_shoe<-as.integer(treedata$brch_shoe=='Yes')
+treedata$sidewalk<-as.integer(treedata$sidewalk=='Damage')
+treedata$guards<-factor(treedata$guards, levels = c("Unsure","Harmful","None","Helpful"),labels= c("1","2","3","4"))
+treedata$guards<-as.integer(treedata$guards)
+treedata$health<-factor(treedata$health, levels = c("Poor","Fair","Good"),labels= c("1","2","3"))
+treedata$health<-as.integer(treedata$health)
+
+####
 TreeProblems<-rbind()
 TreeNOProblems<-rbind()
 
@@ -54,7 +68,7 @@ rownames(TreeNOProblems)<-c('root_stone','root_grate','trnk_wire','trnk_light','
 library(plotly)
 
 rowtree<-rownames(TreeProblems)
-top_labels <- c('Poor', 'Fair', 'Good')
+#top_labels <- c('Poor', 'Fair', 'Good')
 
 HealthData<-data.frame(rowtree,TreeProblems)
 
