ff0a7a91cbe02d61c2acf7cc7f4118d8fccebbd8
HolyMonarch <wb2304@columbia.edu>
2016-10-11 15:41:17 -0400

    Add files via upload

--- app/ui.R
+++ app/ui.R
@@ -1,47 +1,63 @@
 library(shiny)
 library(shinydashboard)
+library(leaflet)
 
-
-shinyUI(dashboardPage(
-  dashboardHeader(title = "Trees in New York"
-                  ),
-  dashboardSidebar(
-    sidebarMenu(
-      menuItem("Data Visualization",tabName="dashboard",icon= icon("dashboard")),
-      menuItem("Clustering Analysis",tabName="Clustering",icon=icon("th"))
-      ),
-    
-    textInput("location","One location:",'Columbia University NY, NYC'),
-#    selectInput("Tree_types","Choose One Category",Types),
-    checkboxGroupInput("Category","Your Favorite Tree:",Types,inline=T),
-    submitButton("Apply Changes")
-    ),
-  dashboardBody(
-    tabItems(
-      tabItem(tabName="dashboard",
-        fluidRow(
-          column(width=9,
-                 box(width=NULL,solidHeader = TRUE,
-              title="New York City",
-              collapsible=T,
-            leafletOutput("map")
-            )),
-          column(width=3,
-                 box(
-                   plotOutput("Tree_Icon")
-                 ),
-                 box(
-                   textOutput("Information")
-                 ),offset=0
-                 )
-          )
-            ),
-      tabItem(tabName="Clustering",
-        fluidRow(
-          box(
-            ))
-        )
-    )
-  )
+shinyUI(navbarPage("Trees in New York",id="nav",
+                   
+  tabPanel("New York",
+           div(class="outer",
+               tags$style(type = "text/css", "#map {height: calc(100vh - 80px) !important;}"),
+               leafletOutput("map",width="100%",height = "100%"),
+               absolutePanel(id="control1",fixed = T,draggable = T,class = "panel panel-default",top = 80,
+                             left = 55, right = "auto", bottom = "auto",
+                             width = 280, height = 550,
+                             h3("Trees"),
+                             helpText("Choose Your Location"),
+                             textInput("location","Your Location:","Columbia University NY"),
+                             actionButton("submit","Mark"),
+                             helpText("Choose Your Favorite Tree"),
+                             checkboxInput("Status","Only Alive"),
+                             selectInput("Tree","Your Favorite Tree",Types, multiple = T),
+#                             textOutput("text"),
+#                             submitButton("Apply Changes"),
+                             plotOutput("barplot")
+                             ))
+            
+  ),
+  tabPanel("EDA",
+           #Need corresponding server part
+           sidebarLayout(position="right",
+             sidebarPanel(
+               conditionalPanel(condition="input.edaPanels==1",
+                                helpText("Fit your model"),
+                                selectInput("variables","Choose one variable:",Variables)),
+               conditionalPanel(condition="input.edaPanels==2",
+                                helpText("Choose the Problem"),
+                                checkboxGroupInput("variables2","Choose at least 2 Components:",Variables))
+             ),
+             mainPanel(
+               tabsetPanel(type="pill",
+                           tabPanel("Ananysis of One Element to Health",
+                                    plotOutput("plot1"),value=1),
+                           tabPanel("Ananysis of Effects between different Elements",
+                                    plotOutput("plot2"),value=2),
+                           id="edaPanels"
+                           )
+             )
+           )),
+  tabPanel("Clustering",
+           #Need corresponding server part
+           sidebarLayout(position="right",
+                         sidebarPanel(
+                           h3("Select the number of clusters"),
+                           selectInput("k","Number of clusters:",choices = list('3','4','5')),
+                           selectInput("method","Methods for Clustering:",choices=list('Kmeans','Hierarchical Clustering'))
+                         ),
+                         mainPanel(
+                           h2("Clustering"),
+                           plotOutput("kmeans")
+                         ))),
+  tabPanel("Text Mining"
+           )
 ))
 
