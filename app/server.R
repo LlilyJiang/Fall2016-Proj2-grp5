96d58ba0cf385d90fb95d9bf5782f9a04a88c7da
monicatao <mt3051@columbia.edu>
2016-10-11 23:13:55 -0400

    Add files via upload

--- app/server.R
+++ app/server.R
@@ -11,37 +11,37 @@ shinyServer(function(input, output) {
       setView(lng = -73.97, lat = 40.70, zoom = 11)
   })
   
-#  address <- reactive({
-#    if(input$submit[1] > 0){
-#      result=data.frame()
-#      url = paste0('http://maps.google.com/maps/api/geocode/xml?address=',input$location,'&sensor=false')
-#      doc = xmlTreeParse(url) 
-#      root = xmlRoot(doc) 
-#      lat = as.numeric(xmlValue(root[['result']][['geometry']][['location']][['lat']])) 
-#      long = as.numeric(xmlValue(root[['result']][['geometry']][['location']][['lng']]))
-#      result=data.frame(latitude=lat,longitude=long)
-#    }
-#    return(result)
-#  })
+  #  address <- reactive({
+  #    if(input$submit[1] > 0){
+  #      result=data.frame()
+  #      url = paste0('http://maps.google.com/maps/api/geocode/xml?address=',input$location,'&sensor=false')
+  #      doc = xmlTreeParse(url) 
+  #      root = xmlRoot(doc) 
+  #      lat = as.numeric(xmlValue(root[['result']][['geometry']][['location']][['lat']])) 
+  #      long = as.numeric(xmlValue(root[['result']][['geometry']][['location']][['lng']]))
+  #      result=data.frame(latitude=lat,longitude=long)
+  #    }
+  #    return(result)
+  #  })
   
   ###Filter the data
   
   TType <- reactive({
     t <- Trees
     
-#    if(length(input$Tree_types)!=0){
-#      t = t[t$category==input$Tree_types,]
-#    }
+    #    if(length(input$Tree_types)!=0){
+    #      t = t[t$category==input$Tree_types,]
+    #    }
     
     if(length(input$Tree)!=0){
       t = t[t$category==input$Tree,]
-#      t = t[t$category=="Oak",]
-#      dim(t)
-#      t = filter(t,t$category == input$Tree)
+      #      t = t[t$category=="Oak",]
+      #      dim(t)
+      #      t = filter(t,t$category == input$Tree)
     }
     if(input$Status==T){
       t = t[t$status== "Alive",]
-#      t = filter(t,t$status=="Alive")
+      #      t = filter(t,t$status=="Alive")
     }
     return(t)
   })
@@ -50,10 +50,10 @@ shinyServer(function(input, output) {
   observe({
     leafletProxy("map") %>%
       clearShapes() %>%
-        addCircles(data = TType(), ~longitude, ~latitude,radius=0.5,opacity=0.5)
+      addCircles(data = TType(), ~longitude, ~latitude,radius=0.5,opacity=0.5)
   })
-#Used to debug  
-#  output$text <- renderText(c(input$Tree,dim(TType())))
+  #Used to debug  
+  #  output$text <- renderText(c(input$Tree,dim(TType())))
   
   
   #Add markers to your location
@@ -63,18 +63,18 @@ shinyServer(function(input, output) {
     root = xmlRoot(doc) 
     lat = as.numeric(xmlValue(root[['result']][['geometry']][['location']][['lat']])) 
     long = as.numeric(xmlValue(root[['result']][['geometry']][['location']][['lng']]))
-
-   leafletProxy("map") %>%
+    
+    leafletProxy("map") %>%
       clearMarkers() %>%
-#      addMarkers(data=address(),~longitude,lat=latitude)
+      #      addMarkers(data=address(),~longitude,lat=latitude)
       addMarkers(lng=long,lat=lat)
-   
-#   loc_zip=as.numeric(xmlValue(xmlChildren(root[['result']][[13]][[1]])$text))
-   
-#   output$barplot <- renderPlot({
-     #zip_data
-     #health, status,species
-#   })
+    
+    #   loc_zip=as.numeric(xmlValue(xmlChildren(root[['result']][[13]][[1]])$text))
+    
+    #   output$barplot <- renderPlot({
+    #zip_data
+    #health, status,species
+    #   })
   })
   
   output$plot1 <- renderPlotly({
@@ -99,35 +99,35 @@ shinyServer(function(input, output) {
   })
   
   # Compute and plot wss for k = 2 to k = 15
-#  k.max <- 15 # Maximal number of clusters
-#  data <- zipdata
-#  wss <- sapply(1:k.max, 
-#                function(k){kmeans(data, k, nstart=15 )$tot.withinss})
-#  fviz_nbclust(zipdata, kmeans, method = "wss") +
-#    geom_vline(xintercept = 6, linetype = 2)
+  #  k.max <- 15 # Maximal number of clusters
+  #  data <- zipdata
+  #  wss <- sapply(1:k.max, 
+  #                function(k){kmeans(data, k, nstart=15 )$tot.withinss})
+  #  fviz_nbclust(zipdata, kmeans, method = "wss") +
+  #    geom_vline(xintercept = 6, linetype = 2)
   
   #Produce the kmeans map
   output$kmeans <- renderPlot({
     zip_data<-data.frame(zip_data)
     
     zipdata<-data.frame(zip_data[,2:15])
-#    zipdata<-data.frame(zipdata)
+    #    zipdata<-data.frame(zipdata)
     #head(zipdata)
-    zipdata[,1:14] <- sapply(zipdata[, 1:14], as.numeric)
+    #zipdata[,1:14] <- sapply(zipdata[, 1:14], as.numeric)
     set.seed(123)
     km.res<-kmeans(zipdata,as.numeric(input$k),nstart = 25)
-#    km.res<-kmeans(zipdata,3,nstart = 25)
-#    fviz_cluster(km.res, data = data.scaled, geom = "point",
-#                stand = FALSE, frame.type = "norm")
-  
-    aggregate(zipdata,by=list(km.res$cluster),FUN=mean)
-  
+    #    km.res<-kmeans(zipdata,3,nstart = 25)
+    #    fviz_cluster(km.res, data = data.scaled, geom = "point",
+    #                stand = FALSE, frame.type = "norm")
+    
+    #aggregate(zipdata,by=list(km.res$cluster),FUN=mean)
+    
     zip_data$kmcluster<-km.res$cluster
-#    Trees$kmcluster<-zip_data$kmcluster[match(Trees$zipcode,zip_data$zipcode)]
+    #    Trees$kmcluster<-zip_data$kmcluster[match(Trees$zipcode,zip_data$zipcode)]
     
     df_pop_zip$value=rep(1,dim(df_pop_zip)[1])
     df_pop_zip[df_pop_zip$region==10025,]
-    df_pop_zip$value <- zip_data$kmcluster[match(df_pop_zip$region,zip_data$Zipcode)]
+    df_pop_zip$value <- zip_data$kmcluster[match(df_pop_zip$region,zip_data$zipcode)]
     # New York City is comprised of 5 counties: Bronx, Kings (Brooklyn), New York (Manhattan), 
     # Queens, Richmond (Staten Island). Their numeric FIPS codes are:
     nyc_fips = c(36005, 36047, 36061, 36081, 36085)
@@ -137,20 +137,20 @@ shinyServer(function(input, output) {
     
     zip_choropleth(df_pop_zip, county_zoom = nyc_fips) + coord_map()
     
-  
-  ########Hierarchical clustering
-  # Compute pairewise distance matrices
-  #  dist.res <- dist(zipdata, method = "euclidean")
-  # Hierarchical clustering results
-  #  hc <- hclust(dist.res, method = "complete")
-  # Visualization of hclust
-  #  plot(hc, labels = FALSE, hang = -1)
-  # Add rectangle around 3 groups
-  #  rect.hclust(hc, k = 5, border = 2:4)
-  
     
-  
-  
+    ########Hierarchical clustering
+    # Compute pairewise distance matrices
+    #  dist.res <- dist(zipdata, method = "euclidean")
+    # Hierarchical clustering results
+    #  hc <- hclust(dist.res, method = "complete")
+    # Visualization of hclust
+    #  plot(hc, labels = FALSE, hang = -1)
+    # Add rectangle around 3 groups
+    #  rect.hclust(hc, k = 5, border = 2:4)
+    
+    
+    
+    
   })
   
   output$summary <-renderPlot({
